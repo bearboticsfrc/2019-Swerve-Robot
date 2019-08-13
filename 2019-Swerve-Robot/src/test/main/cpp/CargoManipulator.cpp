e6b7bc8d6f089d8967c0341a7559ebc13f59865d
Ben Breisch <ben.breisch@gmail.com>
2019-08-13 13:06:51 -0600

    Added Cargo Manipulator Subsystem

---
+++ 2019-Swerve-Robot/src/test/main/cpp/CargoManipulator.cpp
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "CargoManipulator.h"
+
+CargoManipulator::CargoManipulator(int neoPort, int motorPort, int limitPort) :
+  Subsystem("CargoManipulator"),
+  neoMotor(neoPort),
+  otherMotor(motorPort),
+  limitSwitch(limitPort)
+   { 
+     extendManipulator(false);
+     setOutMotorSpeed(0.0);
+   }
+
+void CargoManipulator::InitDefaultCommand() {
+  // Set the default command for a subsystem here.
+  // SetDefaultCommand(new MySpecialCommand());
+}
+void CargoManipulator::setOutMotorSpeed(double speed) {
+  // whatevermotorisintaking.Set(speed);
+}
+
+void CargoManipulator::extendManipulator(bool extend) {
+  //if extend is true
+  //check if limit switch is pressed
+  //if it is: Extend
+  /// if extend is false
+  // reverse motor until limit switch is pressed
+}
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ 2019-Swerve-Robot/src/test/main/include/CargoManipulator.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+#include <frc/DigitalInput.h>
+#include <DigitalInput.h>
+#include <SparkMax.h>
+#include <ctre/Phoenix.h>
+class CargoManipulator : public frc::Subsystem {
+ private:
+ rev::SparkMax neoMotor;
+ WPI_VictorSPX otherMotor;
+ DigitalInput limitSwitch;
+
+ public:
+  CargoManipulator(int neoPort, int motorPort, int limitPort);
+  void InitDefaultCommand() override;
+  void setOutMotorSpeed(double speed);
+  void extendManipulator(bool extend);
+};
