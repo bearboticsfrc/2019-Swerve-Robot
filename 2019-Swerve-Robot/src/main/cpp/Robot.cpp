174a54bfc834452ff59c13cbb627d04ece60017e
bbreisch26 <43624702+bbreisch26@users.noreply.github.com>
2019-08-15 21:22:15 -0600

    More fixes

--- 2019-Swerve-Robot/src/main/cpp/Robot.cpp
+++ 2019-Swerve-Robot/src/main/cpp/Robot.cpp
@@ -53,14 +53,6 @@ void Robot::RobotInit() {
   frc::SmartDashboard::init();
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
 void Robot::RobotPeriodic() {
   for (int i = 0; i < m_swerveModules.size(); ++i) {
     double lastAngle = frc::SmartDashboard::GetNumber("Swerve Angle " + std::to_string(i), m_swerveModules[i]->getAngle());
@@ -74,18 +66,8 @@ void Robot::RobotPeriodic() {
   }
 }
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
 void Robot::AutonomousInit() {
+  /*
   m_autoSelected = m_chooser.GetSelected();
   // m_autoSelected = SmartDashboard::GetString(
   //     "Auto Selector", kAutoNameDefault);
@@ -96,14 +78,19 @@ void Robot::AutonomousInit() {
   } else {
     // Default Auto goes here
   }
+  */
+ Robot::TeleopInit();
 }
 
 void Robot::AutonomousPeriodic() {
+  /*
   if (m_autoSelected == kAutoNameCustom) {
     // Custom Auto goes here
   } else {
     // Default Auto goes here
   }
+  */
+ Robot::TeleopPeriodic();
 }
 
 void Robot::TeleopInit() {
